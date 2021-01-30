cd33acd0c5ce170d990e9e9860700b753752b7c4
Mark B <sn1572@gmail.com>
2021-01-30 17:11:50 -0500

    added 324

---
+++ 324_wiggle_sort_ii.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 30 16:24:07 2021
+
+@author: User68
+"""
+
+class Solution:
+
+    def sort_pairs(self, nums):
+        '''
+        
+        '''
+
+    def wiggleSort(self, nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
